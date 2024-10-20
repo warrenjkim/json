edac46a1f1e8eb1d7dacaaa0615afd3adb7a0986
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-10-20 15:00:31 -0700

    Add GetVisitor
    * Add ReturnVisitor to all nodes.

---
+++ include/visitors/get_visitor.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ret_visitor.h"
+#include "utils/queue.h"
+
+namespace json {
+
+namespace visitors {
+
+class GetVisitor : public ReturnVisitor {
+ public:
+  GetVisitor(Type* root, const utils::Queue<std::string>& keys);
+  GetVisitor(const Type* root, const utils::Queue<std::string>& keys);
+  Type* visit(const Array& node) override;
+  Type* visit(const Boolean& node) override;
+  Type* visit(const Null& node) override;
+  Type* visit(const Number& node) override;
+  Type* visit(const Object& node) override;
+  Type* visit(const String& node) override;
+
+ private:
+  Type* ast_;
+  utils::Queue<std::string> keys_;
+};
+
+}  // namespace visitors
+
+}  // namespace json
