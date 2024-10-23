8c5b06e989bb4cf17ce9f42848372c82be63d48d
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-10-23 15:37:31 -0700

    Fix json_utils

--- include/utils/json_utils.h
+++ include/utils/json_utils.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <string>
 #include <string_view>
 
-#include "json/json.h"
 #include "nodes/node.h"
 #include "parse/token.h"
 #include "utils/queue.h"
@@ -15,9 +13,5 @@ std::optional<utils::Queue<Token>> tokenize(std::string_view json);
 Node* parse(const std::string_view json);
 
 Node* parse(utils::Queue<Token>& tokens);
-
-Json jsonify(const std::string& json);
-
-std::string stringify(const Json& json);
 
 }  // namespace json
