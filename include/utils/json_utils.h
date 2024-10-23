eb7b6a07bef6a7e4254cc7488360fc5f4b9b173f
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-10-23 15:34:26 -0700

    Update json_utils

--- include/utils/json_utils.h
+++ include/utils/json_utils.h
@@ -1,17 +1,23 @@
 #pragma once
 
+#include <string>
 #include <string_view>
 
+#include "json/json.h"
+#include "nodes/node.h"
 #include "parse/token.h"
-#include "types/type.h"
 #include "utils/queue.h"
 
 namespace json {
 
 std::optional<utils::Queue<Token>> tokenize(std::string_view json);
 
-Type* parse(const std::string_view json);
+Node* parse(const std::string_view json);
 
-Type* parse(utils::Queue<Token>& tokens);
+Node* parse(utils::Queue<Token>& tokens);
+
+Json jsonify(const std::string& json);
+
+std::string stringify(const Json& json);
 
 }  // namespace json
