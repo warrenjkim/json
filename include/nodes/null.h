d39343395380d915b18ad2ce097a456152380f0f
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-11-09 22:55:09 -0800

    Add ptr() to all nodes

--- include/nodes/null.h
+++ include/nodes/null.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include "node.h"
 
 namespace json {
@@ -8,6 +9,9 @@ class Null : public Node {
  public:
   void accept(visitors::Visitor& visitor) override;
   void accept(visitors::ConstVisitor& visitor) const override;
+
+public:
+  operator std::nullptr_t() const;
 };
 
 }  // namespace json
