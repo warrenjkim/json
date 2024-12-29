c37887866d3f982cad80c74dd3feffef40b7f6e0
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-12-29 13:30:10 -0800

    add test

--- tests/json/value_test.cc
+++ tests/json/value_test.cc
@@ -4,6 +4,7 @@
 
 #include <boost/log/trivial.hpp>
 #include <cstddef>
+#include <stdexcept>  // out_of_range
 #include <string>
 
 #include "json/exception.h"
@@ -521,11 +522,34 @@ TEST_F(ValueTest, ValueAssignment) {
   ASSERT_EQ(string_value, "some_string");
 }
 
-TEST_F(ValueTest, ChangesPropagateToCache) {
+TEST_F(ValueTest, ObjectChangesPropagateToCache) {
   json::Value val;
   val["key"] = 10;
+  ASSERT_EQ(val["key"], 10);
+
   val["key"] = "10";
   ASSERT_EQ(val["key"], "10");
+
+  val["key"] = false;
+  ASSERT_EQ(val["key"], false);
+}
+
+TEST_F(ValueTest, ArrayChangesPropagateToCache) {
+  json::Value val;
+  val.add(10);
+  ASSERT_EQ(val[0], 10);
+
+  val[0] = "20";
+  ASSERT_EQ(val[0], "20");
+
+  val[0] = std::string("30");
+  ASSERT_EQ(val[0], "30");
+
+  val[0] = false;
+  ASSERT_EQ(val[0], false);
+
+  val[0] = 20;
+  ASSERT_EQ(val[0], 20);
 }
 
 TEST_F(ValueTest, AssignNull) {
