2669012524bf6c867b9770858911cb067fac43ba
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-04-26 21:46:21 -0700

    add unhappy path tests

--- tests/parse/lexer_test.cc
+++ tests/parse/lexer_test.cc
@@ -5,6 +5,27 @@
 
 class LexerTest : public ::testing::Test {};
 
+TEST_F(LexerTest, LexInvalidKeywords) {
+  {  // incomplete null
+    json::syntax::Lexer lexer("nul");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "nul");
+  }
+  {  // incomplete true
+    json::syntax::Lexer lexer("tru");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "tru");
+  }
+  {  // incomplete false
+    json::syntax::Lexer lexer("fals");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "fals");
+  }
+}
+
 TEST_F(LexerTest, LexNull) {
   json::syntax::Lexer lexer("null");
   json::syntax::Token token = lexer.next_token();
@@ -27,6 +48,27 @@ TEST_F(LexerTest, LexBoolean) {
   }
 }
 
+TEST_F(LexerTest, LexUnterminatedString) {
+  {  // unterminated string
+    json::syntax::Lexer lexer("\"hello");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "hello");
+  }
+  {  // bad escape char
+    json::syntax::Lexer lexer("\"hello\\z\"");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "hello\\z");
+  }
+  {  // bad unicode
+    json::syntax::Lexer lexer("\"\\u12\"");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "\\u12");
+  }
+}
+
 TEST_F(LexerTest, LexString) {
   {  // simple
     json::syntax::Lexer lexer("\"hello\"");
@@ -43,6 +85,39 @@ TEST_F(LexerTest, LexString) {
     json::syntax::Lexer lexer("\"\\u0041\"");
     json::syntax::Token token = lexer.next_token();
     EXPECT_EQ(token.value, "A");
+  }
+}
+
+TEST_F(LexerTest, LexInvalidNumbers) {
+  {  // -
+    json::syntax::Lexer lexer("-");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "-");
+  }
+  {  // 01
+    json::syntax::Lexer lexer("01");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "01");
+  }
+  {  // 1.
+    json::syntax::Lexer lexer("1.");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "1.");
+  }
+  {  // 1e
+    json::syntax::Lexer lexer("1e");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "1e");
+  }
+  {  // 1e+
+    json::syntax::Lexer lexer("1e+");
+    json::syntax::Token token = lexer.next_token();
+    EXPECT_EQ(token.type, json::syntax::TokenType::UNKNOWN);
+    EXPECT_EQ(token.value, "1e+");
   }
 }
 
@@ -64,10 +139,9 @@ TEST_F(LexerTest, LexPunctuation) {
   };
 
   std::vector<std::string> inputs = {"{", "}", "[", "]", ",", ":"};
-
-  for (size_t i = 0; i < inputs.size(); ++i) {
-    json::syntax::Lexer lexer(inputs[i]);
-    json::syntax::Token token = lexer.next_token();
+  json::syntax::Lexer lexer("{}[],:");
+  for (size_t i = 0; i < inputs.size(); i++) {
+    json::syntax::Token token = *(++lexer);
     EXPECT_EQ(token, expected[i]);
   }
 }
