17ea2f8c5bd8ca860dd1ebf75be3e2f227860def
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-05-15 21:14:34 -0700

    integral type

---
+++ include/warren/internal/dsa/numeric.h
@@ -0,0 +1,31 @@
+#include <cstdint>
+
+namespace json {
+
+namespace dsa {
+
+struct Integral {
+  enum {
+    UNSET,
+    INT8,   // char
+    INT16,  // short
+    INT32,  // int
+    INT64   // long long
+  } type;
+
+  union {
+    int8_t i8;
+    int16_t i16;
+    int32_t i32;
+    int64_t i64;
+
+    // used only during parsing
+    uint64_t accumulator;
+  };
+
+  Integral() : i8(0), type(UNSET) {}
+};
+
+}  // namespace dsa
+
+}  // namespace json
---
+++ src/dsa/numeric.cc
@@ -0,0 +1,90 @@
+#include "warren/internal/dsa/numeric.h"
+
+#include <string>
+#include <string_view>
+
+namespace json {
+
+namespace dsa {
+
+Integral to_integral(std::string_view sv) {
+  Integral res;
+  bool negative = false;
+  for (const char c : sv) {
+    if (c == '-') {
+      negative = true;
+      continue;
+    } else if (c == '+') {
+      continue;
+    }
+
+    int8_t digit = c - '0';
+    switch (res.type) {
+      case Integral::UNSET:
+        res.accumulator += digit;
+        break;
+      case Integral::INT8:
+        res.accumulator *= 10;
+        res.accumulator += digit;
+        if ((!negative && res.accumulator > INT8_MAX) ||
+            (negative && res.accumulator > -INT8_MIN)) {
+          res.type = Integral::INT16;
+        }
+
+        break;
+      case Integral::INT16:
+        res.accumulator *= 10;
+        res.accumulator += digit;
+        if ((!negative && res.accumulator > INT16_MAX) ||
+            (negative && res.accumulator > -INT16_MIN)) {
+          res.type = Integral::INT32;
+        }
+
+        break;
+      case Integral::INT32:
+        res.accumulator *= 10;
+        res.accumulator += digit;
+        if ((!negative && res.accumulator > INT32_MAX) ||
+            (negative && res.accumulator > uint32_t(INT32_MAX) + 1)) {
+          res.type = Integral::INT64;
+        }
+
+        break;
+      case Integral::INT64:
+        if ((!negative && res.accumulator > (INT64_MAX - digit) / 10) ||
+            (negative &&
+             res.accumulator > (uint64_t(INT64_MAX) + 1 - digit) / 10)) {
+          throw std::out_of_range("Number " + std::string(sv) +
+                                  " is out of range of int64_t limits");
+        }
+
+        res.accumulator *= 10;
+        res.accumulator += digit;
+
+        break;
+    }
+  }
+
+  switch (res.type) {
+    case Integral::UNSET:
+      break;
+    case Integral::INT8:
+      res.i8 = (-1 * (-1 * negative)) * int8_t(res.accumulator);
+      break;
+    case Integral::INT16:
+      res.i16 = (-1 * (-1 * negative)) * int16_t(res.accumulator);
+      break;
+    case Integral::INT32:
+      res.i32 = (-1 * (-1 * negative)) * int32_t(res.accumulator);
+      break;
+    case Integral::INT64:
+      res.i64 = (-1 * (-1 * negative)) * int64_t(res.accumulator);
+      break;
+  }
+
+  return res;
+}
+
+}  // namespace dsa
+
+}  // namespace json
