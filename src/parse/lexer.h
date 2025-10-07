e17103a37925c6798e12070fdba88dec14eececc
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-10-06 17:46:19 -0700

    to_string

--- src/parse/lexer.h
+++ src/parse/lexer.h
@@ -22,46 +22,6 @@ class Lexer {
       return expected == other.expected && pos == other.pos &&
              message == other.message;
     }
-
-    operator std::string() {
-      auto type = [this]() -> std::string {
-        switch (expected) {
-          case TokenType::OBJECT_START:
-            return "TokenType::ObjectStart";
-          case TokenType::OBJECT_END:
-            return "TokenType::ObjectEnd";
-          case TokenType::ARRAY_START:
-            return "TokenType::ArrayStart";
-          case TokenType::ARRAY_END:
-            return "TokenType::ArrayEnd";
-          case TokenType::QUOTE:
-            return "TokenType::Quote";
-          case TokenType::COMMA:
-            return "TokenType::Comma";
-          case TokenType::COLON:
-            return "TokenType::Colon";
-          case TokenType::STRING:
-            return "TokenType::String";
-          case TokenType::DOUBLE:
-            return "TokenType::Double";
-          case TokenType::INTEGRAL:
-            return "TokenType::Integral";
-          case TokenType::BOOLEAN:
-            return "TokenType::Boolean";
-          case TokenType::JSON_NULL:
-            return "TokenType::Null";
-          case TokenType::UNKNOWN:
-            return "TokenType::Unknown";
-          case TokenType::END_OF_JSON:
-            return "TokenType::EndOfJson";
-        }
-
-        __builtin_unreachable();
-      };
-
-      return "Error at position " + std::to_string(pos) + ". Expected " +
-             type() + ": " + message;
-    }
   };
 
   explicit Lexer(const std::string& json);
@@ -102,6 +62,17 @@ class Lexer {
   Token curr_;
   std::optional<Error> error_;
 };
+
+inline std::string to_string(const Lexer::Error& error) {
+  std::string msg = "Error at position " + std::to_string(error.pos) + ". ";
+  if (error.expected != TokenType::UNKNOWN) {
+    msg += "Expected " + to_string(error.expected) + ": ";
+  }
+
+  msg += error.message;
+
+  return msg;
+}
 
 }  // namespace json
 }  // namespace warren
